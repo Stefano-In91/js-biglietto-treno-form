8ef33ce87666db19439bf2907c6f59d1961a334d
Stefano.In91 <ste.ince91@gmail.com>
2022-10-21 17:19:41 +0200

    adds js to input data to html ticket

--- js/app.js
+++ js/app.js
@@ -4,40 +4,45 @@ const generate = document.getElementById("generate-btn");
 
 generate.addEventListener('click', function() {
 
-// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
+// Document.Get per prendere i dati inseriti negli input richiesti
    let firstName = document.getElementById("first-name").value;
    let lastName = document.getElementById("last-name").value;
-
    let runKm = Number(document.getElementById("km").value);
-
    let passengerAge = document.getElementById("age").value;
-
-   console.log(typeof firstName, firstName)
-   console.log(typeof lastName, lastName)
-   console.log(typeof runKm, runKm)
-   console.log(typeof passengerAge, passengerAge)
 
 // Prezzo del biglietto definito in base ai km (0.21 € al km).
    const pricePerKm = 0.21;
    let runPrice = runKm * pricePerKm;
 
-   console.log(`${runPrice} prezzo corsa iniziale`);
+   let ticketPricing = "Biglietto standard";
 
 // Calcolo sconti per fasce d'età
    if ( passengerAge === "underage" ) {
       // Applicato uno sconto del 20% per i minorenni.
+      ticketPricing = "Biglietto Minorenni";
       runPrice = runPrice * 0.8;
-      console.log( `${runPrice} prezzo corsa dopo lo sconto` );
    } else if ( passengerAge === "over_age" ) {
       // Applicato uno sconto del 40% per gli over 65.
+      ticketPricing = "Biglietto Over-65";
       runPrice = runPrice * 0.6;
-      console.log( `${runPrice} prezzo corsa dopo lo sconto` );
    }
-
-// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+// L'output prezzo finale arrotondato a 2 cifre decimali
    runPrice = parseFloat(runPrice).toFixed(2);
-   console.log( `${runPrice} prezzo corsa finale` );
 
+// Inserimento valori in html
+   // Cognome + Nome
+   document.querySelector("th.ticket-data").innerHTML = `${lastName} ${firstName}`;
+   // Tariffa applicata
+   document.querySelector("td.ticket-data:nth-of-type(1)").innerHTML = `${ticketPricing}`;
+   // Numero carrozza casuale 1-10
+   let coachNum = Math.floor(Math.random() * 10 +1);
+   document.querySelector("td.ticket-data:nth-of-type(2)").innerHTML = `${coachNum}`;
+   // Codice CP 90000-99999
+   let cpNum = Math.floor(Math.random() * 9999 + 90000);
+   document.querySelector("td.ticket-data:nth-of-type(3)").innerHTML = `${cpNum}`;
+   // Prezzo
+   document.querySelector("td.ticket-data:nth-of-type(3)").innerHTML = `${runPrice}`;
 
+// Show Ticket su Click
+   
 })
